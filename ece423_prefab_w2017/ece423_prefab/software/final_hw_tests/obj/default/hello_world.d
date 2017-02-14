d7a04a9c3fe03a567e93bb5ad839a5403568720b
Harley Armstrong <hjarmstr@uwaterloo.ca>
2017-02-13 22:33:42 -0500

    Nonfunctional reset

---
+++ ece423_prefab_w2017/ece423_prefab/software/final_hw_tests/obj/default/hello_world.d
@@ -0,0 +1,60 @@
+obj/default/hello_world.o: hello_world.c \
+ ../final_hw_tests_bsp//HAL/inc/io.h \
+ ../final_hw_tests_bsp//HAL/inc/alt_types.h \
+ ../final_hw_tests_bsp//HAL/inc/sys/alt_cache.h \
+ ../final_hw_tests_bsp//HAL/inc/alt_types.h \
+ ../final_hw_tests_bsp//HAL/inc/priv/alt_file.h \
+ ../final_hw_tests_bsp//HAL/inc/sys/alt_dev.h \
+ ../final_hw_tests_bsp/system.h ../final_hw_tests_bsp/linker.h \
+ ../final_hw_tests_bsp//HAL/inc/sys/alt_llist.h \
+ ../final_hw_tests_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../final_hw_tests_bsp//HAL/inc/os/alt_sem.h \
+ ../final_hw_tests_bsp//HAL/inc/priv/alt_no_error.h \
+ ../final_hw_tests_bsp//drivers/inc/altera_msgdma.h \
+ ../final_hw_tests_bsp//drivers/inc/altera_msgdma_csr_regs.h \
+ ../final_hw_tests_bsp//drivers/inc/altera_msgdma_descriptor_regs.h \
+ ../final_hw_tests_bsp//drivers/inc/altera_msgdma_response_regs.h \
+ ../final_hw_tests_bsp//drivers/inc/altera_msgdma_prefetcher_regs.h \
+ ../final_hw_tests_bsp//HAL/inc/os/alt_flag.h \
+ ../final_hw_tests_bsp//drivers/inc/altera_msgdma_descriptor_regs.h \
+ ../final_hw_tests_bsp//drivers/inc/altera_msgdma_csr_regs.h
+
+../final_hw_tests_bsp//HAL/inc/io.h:
+
+../final_hw_tests_bsp//HAL/inc/alt_types.h:
+
+../final_hw_tests_bsp//HAL/inc/sys/alt_cache.h:
+
+../final_hw_tests_bsp//HAL/inc/alt_types.h:
+
+../final_hw_tests_bsp//HAL/inc/priv/alt_file.h:
+
+../final_hw_tests_bsp//HAL/inc/sys/alt_dev.h:
+
+../final_hw_tests_bsp/system.h:
+
+../final_hw_tests_bsp/linker.h:
+
+../final_hw_tests_bsp//HAL/inc/sys/alt_llist.h:
+
+../final_hw_tests_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../final_hw_tests_bsp//HAL/inc/os/alt_sem.h:
+
+../final_hw_tests_bsp//HAL/inc/priv/alt_no_error.h:
+
+../final_hw_tests_bsp//drivers/inc/altera_msgdma.h:
+
+../final_hw_tests_bsp//drivers/inc/altera_msgdma_csr_regs.h:
+
+../final_hw_tests_bsp//drivers/inc/altera_msgdma_descriptor_regs.h:
+
+../final_hw_tests_bsp//drivers/inc/altera_msgdma_response_regs.h:
+
+../final_hw_tests_bsp//drivers/inc/altera_msgdma_prefetcher_regs.h:
+
+../final_hw_tests_bsp//HAL/inc/os/alt_flag.h:
+
+../final_hw_tests_bsp//drivers/inc/altera_msgdma_descriptor_regs.h:
+
+../final_hw_tests_bsp//drivers/inc/altera_msgdma_csr_regs.h:
