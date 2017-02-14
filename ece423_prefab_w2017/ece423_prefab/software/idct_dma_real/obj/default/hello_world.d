9a9eefd8aa5a7c46e446d258b7034e3b46d7ccf8
unknown <t53wu@ECE-CPUIO25.NEXUS.UWATERLOO.CA>
2017-02-13 23:47:41 -0500

    Software for dma colour block in idct_dma_real. wip

---
+++ ece423_prefab_w2017/ece423_prefab/software/idct_dma_real/obj/default/hello_world.d
@@ -0,0 +1,63 @@
+obj/default/hello_world.o: hello_world.c \
+ ../idct_dma_real_bsp//HAL/inc/io.h \
+ ../idct_dma_real_bsp//HAL/inc/alt_types.h \
+ ../idct_dma_real_bsp//HAL/inc/sys/alt_cache.h \
+ ../idct_dma_real_bsp//HAL/inc/alt_types.h \
+ ../idct_dma_real_bsp//HAL/inc/priv/alt_file.h \
+ ../idct_dma_real_bsp//HAL/inc/sys/alt_dev.h \
+ ../idct_dma_real_bsp/system.h ../idct_dma_real_bsp/linker.h \
+ ../idct_dma_real_bsp//HAL/inc/sys/alt_llist.h \
+ ../idct_dma_real_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../idct_dma_real_bsp//HAL/inc/os/alt_sem.h \
+ ../idct_dma_real_bsp//HAL/inc/priv/alt_no_error.h \
+ ../idct_dma_real_bsp//drivers/inc/altera_msgdma.h \
+ ../idct_dma_real_bsp//drivers/inc/altera_msgdma_csr_regs.h \
+ ../idct_dma_real_bsp//drivers/inc/altera_msgdma_descriptor_regs.h \
+ ../idct_dma_real_bsp//drivers/inc/altera_msgdma_response_regs.h \
+ ../idct_dma_real_bsp//drivers/inc/altera_msgdma_prefetcher_regs.h \
+ ../idct_dma_real_bsp//HAL/inc/os/alt_flag.h \
+ ../idct_dma_real_bsp//drivers/inc/altera_msgdma_descriptor_regs.h \
+ ../idct_dma_real_bsp//drivers/inc/altera_msgdma_csr_regs.h \
+ common/mjpeg423_types.h
+
+../idct_dma_real_bsp//HAL/inc/io.h:
+
+../idct_dma_real_bsp//HAL/inc/alt_types.h:
+
+../idct_dma_real_bsp//HAL/inc/sys/alt_cache.h:
+
+../idct_dma_real_bsp//HAL/inc/alt_types.h:
+
+../idct_dma_real_bsp//HAL/inc/priv/alt_file.h:
+
+../idct_dma_real_bsp//HAL/inc/sys/alt_dev.h:
+
+../idct_dma_real_bsp/system.h:
+
+../idct_dma_real_bsp/linker.h:
+
+../idct_dma_real_bsp//HAL/inc/sys/alt_llist.h:
+
+../idct_dma_real_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../idct_dma_real_bsp//HAL/inc/os/alt_sem.h:
+
+../idct_dma_real_bsp//HAL/inc/priv/alt_no_error.h:
+
+../idct_dma_real_bsp//drivers/inc/altera_msgdma.h:
+
+../idct_dma_real_bsp//drivers/inc/altera_msgdma_csr_regs.h:
+
+../idct_dma_real_bsp//drivers/inc/altera_msgdma_descriptor_regs.h:
+
+../idct_dma_real_bsp//drivers/inc/altera_msgdma_response_regs.h:
+
+../idct_dma_real_bsp//drivers/inc/altera_msgdma_prefetcher_regs.h:
+
+../idct_dma_real_bsp//HAL/inc/os/alt_flag.h:
+
+../idct_dma_real_bsp//drivers/inc/altera_msgdma_descriptor_regs.h:
+
+../idct_dma_real_bsp//drivers/inc/altera_msgdma_csr_regs.h:
+
+common/mjpeg423_types.h:
